a960000215fe74e4aa2f2c7fad0089a7c282dd5f
ZhouYafei <leisure-zyf@qq.com>
2025-01-11 22:52:18 +0800

    手写内容

---
+++ hanhan/提取文字.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import numpy as np
+
+
+im = Image.open('./temp/text.jpg')
+arr_rbg = np.array(im)
+arr_rbga = np.zeros([arr_rbg.shape[0], arr_rbg.shape[1], 4], dtype=np.uint8)
+print(arr_rbg.shape, arr_rbga.shape)
+
+for i in range(arr_rbg.shape[0]):
+    for j in range(arr_rbg.shape[1]):
+        r, g, b = arr_rbg[i, j]
+        # if r+g+b > 300:
+        #     arr_rbga[i, j] = (0, 0, 0, 0)
+        if r < 50 and b < 50 and g < 50:
+            arr_rbga[i, j] = (223, 118, 59, 255)
+        else:
+            arr_rbga[i, j] = (0, 0, 0, 0)
+
+im = Image.fromarray(arr_rbga)
+# im.show()
+im.save('./temp/text-content.png')
